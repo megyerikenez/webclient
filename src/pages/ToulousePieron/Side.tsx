efc8544cb07b46b95d15e7fdeaeabaf947f1846c
megyerikenez <m.kenz99@gmail.com>
2022-10-01 18:42:14 +0200

    ts linter fix

--- src/pages/ToulousePieron/Side.tsx
+++ src/pages/ToulousePieron/Side.tsx
@@ -1,9 +1,22 @@
-export function Side(){
-    return <>
-        <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M32 16H18C16.8954 16 16 16.8954 16 18V32C16 33.1046 16.8954 34 18 34H32C33.1046 34 34 33.1046 34 32V18C34 16.8954 33.1046 16 32 16Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <rect x="24" y="5" width="2" height="11.4141" rx="1" fill="black"/>
-        </svg>
-
+export function Side() {
+  return (
+    <>
+      <svg
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M32 16H18C16.8954 16 16 16.8954 16 18V32C16 33.1046 16.8954 34 18 34H32C33.1046 34 34 33.1046 34 32V18C34 16.8954 33.1046 16 32 16Z"
+          stroke="black"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <rect x="24" y="5" width="2" height="11.4141" rx="1" fill="black" />
+      </svg>
     </>
+  );
 }
