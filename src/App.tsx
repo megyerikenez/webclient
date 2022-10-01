1492f70f8eed2e34a4d02a3f71c302ea666cbf76
Gábor Kriskó <gabor.krisko@gmail.com>
2022-10-01 17:39:37 +0200

    toulouse-pieron

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./css/App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyToJobMockup from "./pages/ApplyToJobMockUp";
+import { ToulousePieron } from "./pages/ToulousePieron";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="mockup" element={<ApplyToJobMockup />} />
+      <Route path="/mockup" element={<ApplyToJobMockup />} />
+      <Route path="/tests/toulouse-pieron" element={<ToulousePieron />} />
     </Routes>
   );
 }
