028a016627de66fe531c5260a533fa0988ddc56c
Gábor Kriskó <gabor.krisko@gmail.com>
2022-10-02 14:13:39 +0200

    refactoring, design

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./css/App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyToJobMockup from "./pages/ApplyToJobMockUp";
-import { ToulousePieron } from "./pages/ToulousePieron";
+import { ToulousePieronPage } from "./pages/ToulousePieron";
 import Settings from "./pages/Settings";
 import Results from "./pages/ApplyToJobMockUp copy";
 import Tests from "./pages/Tests";
@@ -15,7 +15,7 @@ function App() {
       <Route path="/results" element={<Results />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/mockup" element={<ApplyToJobMockup />} />
-      <Route path="/tests/toulouse-pieron" element={<ToulousePieron />} />
+      <Route path="/tests/toulouse-pieron" element={<ToulousePieronPage />} />
     </Routes>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Transition } from "@headlessui/react";
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <div className="sticky top-0 z-50">
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
