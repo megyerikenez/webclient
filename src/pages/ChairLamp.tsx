6b2e98faaa0aa063aaa0d7c9030f1728b915ba1d
megyerikenez <m.kenz99@gmail.com>
2022-10-05 18:07:23 +0200

    Translations,ESLINT fixes

--- src/pages/ChairLamp.tsx
+++ src/pages/ChairLamp.tsx
@@ -1,122 +1,150 @@
-import { COL_COUNT, MAX_TIME, ROW_COUNT } from './ToulousePieron/Constants';
-import { Picture, getImgRes } from './ChairLamp/Picture';
-import React, { useEffect, useState } from 'react';
+import { MAX_TIME } from "./ToulousePieron/Constants";
+import { Picture, getImgRes } from "./ChairLamp/Picture";
 
-import { Corner } from './ToulousePieron/Corner';
-import { GridItem } from './ToulousePieron/GridItem';
-import Navbar from '../components/Navbar';
-import { Score } from './ToulousePieron/Score';
-import { Side } from './ToulousePieron/Side';
-import { Timer } from '../components/Timer';
-import { formatTime } from '../util';
-import shallow from 'zustand/shallow'
-import { useTestStore } from './ChairLamp/store';
+import Navbar from "../components/Navbar";
+import { Score } from "./ToulousePieron/Score";
+import { Timer } from "../components/Timer";
+import shallow from "zustand/shallow";
+import { useTestStore } from "./ChairLamp/store";
+import { useTranslation } from "react-i18next";
 
-export function ChairLampPage(){
-    return <>
+export function ChairLampPage() {
+  return (
+    <>
       <Navbar />
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <ChairLamp />
         </div>
       </main>
-    
     </>
+  );
 }
 
-export function ChairLamp(){
-    let store = useTestStore(({startTime, endTime, hasStarted, hasEnded, endTest, startTest, toggleMarked, pictures, picturesToFind}) => ({
-        startTime, endTime, hasStarted, hasEnded, endTest, startTest, toggleMarked, pictures, picturesToFind
-    }), shallow);
+export function ChairLamp() {
+  const [t, i18n] = useTranslation("common");
+  let store = useTestStore(
+    ({
+      startTime,
+      endTime,
+      hasStarted,
+      hasEnded,
+      endTest,
+      startTest,
+      toggleMarked,
+      pictures,
+      picturesToFind,
+    }) => ({
+      startTime,
+      endTime,
+      hasStarted,
+      hasEnded,
+      endTest,
+      startTest,
+      toggleMarked,
+      pictures,
+      picturesToFind,
+    }),
+    shallow
+  );
 
-    if (!store.hasStarted || store.startTime == null){
-        let squares: number[] = [];
-        for(let i = 0; i < 8; i++){
-            if (i % 2 == 0){
-                squares.push(i);
-            } else {
-                squares.unshift(i);
-            }
-        }
-
-        return <>
-            <div className='px-6'>
-                <h1 className='mb-6'>Toulouse-Piéron teszt</h1>
-                <p>A következő feladatban kapni fog 4 féle négyzetet, azokat kell megtalálnia, és bejelölnie (kattintással).</p>
-                <p>
-					Összesen { getImgRes().length } fajta négyzet van, azok az alábbi módon néznek ki:
-				</p>
-                <div className='my-2'>
-                    {
-						getImgRes().map((res)=>{
-							return <>
-								<Picture res={res} />
-							</>
-						})
-					}
-                </div>
-                <p>A feladat megoldására 5 perc áll rendelkezésre.</p>
-                <button
-                    className='mt-6 button-primary'
-                    onClick={() => {
-                        store.startTest();
-                    }}
-                >Kezdés</button>
-            </div>
-        </>
+  if (!store.hasStarted || store.startTime == null) {
+    let squares: number[] = [];
+    for (let i = 0; i < 8; i++) {
+      if (i % 2 === 0) {
+        squares.push(i);
+      } else {
+        squares.unshift(i);
+      }
     }
 
-    let hasEnded = store.hasEnded && store.endTime;    
-
-    return <>
-        <div>
-            {
-                hasEnded ? <>
-                    <Score />
-                </>
-                :
+    return (
+      <>
+        <div className="px-6">
+          <h1 className="mb-6">{t("tests.chairLamp.name")}</h1>
+          <p>
+            {t("tests.chairLamp.description.1")}A következő feladatban kapni fog
+            4 féle négyzetet, azokat kell megtalálnia, és bejelölnie
+            (kattintással).
+          </p>
+          <p>
+            Összesen {getImgRes().length} fajta négyzet van, azok az alábbi
+            módon néznek ki:
+          </p>
+          <div className="my-2">
+            {getImgRes().map((res) => {
+              return (
                 <>
-                    <div
-                        style={{
-                            flexDirection: 'row',
-                            alignItems: 'start'
-                        }}
-                        className='flex mb-6 gap-6 mx-6 sm:mx-0'
-                    >
-                        <div
-                            className='card text-center pb-2'
-                        >
-                            <p>Az alábbi alakzatokat kell bejelölnie</p>
-                            { store.picturesToFind.map(index =>{
-                                return <>
-                                    <Picture idx={index} />
-                                </>
-                            }) }
-                        </div>
-                        <Timer startTime={store.startTime} maxTime={MAX_TIME} />
-                    </div>
+                  <Picture res={res} />
                 </>
-            }
+              );
+            })}
+          </div>
+          <p>A feladat megoldására 5 perc áll rendelkezésre.</p>
+          <button
+            className="mt-6 button-primary"
+            onClick={() => {
+              store.startTest();
+            }}
+          >
+            Kezdés
+          </button>
+        </div>
+      </>
+    );
+  }
 
-            
-            
+  let hasEnded = store.hasEnded && store.endTime;
+
+  return (
+    <>
+      <div>
+        {hasEnded ? (
+          <>
+            <Score />
+          </>
+        ) : (
+          <>
             <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'stretch',
-                }}
+              style={{
+                flexDirection: "row",
+                alignItems: "start",
+              }}
+              className="flex mb-6 gap-6 mx-6 sm:mx-0"
             >
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 5,
-                    }}
-                >
-                    {store.pictures.map((row, index)=>{
-                        return <>
-                        {/* <span
+              <div className="card text-center pb-2">
+                <p>Az alábbi alakzatokat kell bejelölnie</p>
+                {store.picturesToFind.map((index) => {
+                  return (
+                    <>
+                      <Picture idx={index} />
+                    </>
+                  );
+                })}
+              </div>
+              <Timer startTime={store.startTime} maxTime={MAX_TIME} />
+            </div>
+          </>
+        )}
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "stretch",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 5,
+            }}
+          >
+            {store.pictures.map((row, index) => {
+              return (
+                <>
+                  {/* <span
                             className='font-sm w-8'
                             style={{
                                 display: 'flex',
@@ -126,39 +154,58 @@ export function ChairLamp(){
                                 flexGrow: 1,
                             }}
                         >{index+1}</span> */}
-                        </>
-                    })}
-                </div>
+                </>
+              );
+            })}
+          </div>
 
-                <div>
-                {
-					store.pictures.map(pic => {
-						return <Picture idx={pic.pictureIdx} revised={pic.revised} selected={pic.selected} />
-					})
-				}
-                </div>
-            </div>
+          <div>
+            {store.pictures.map((pic) => {
+              return (
+                <Picture
+                  idx={pic.pictureIdx}
+                  revised={pic.revised}
+                  selected={pic.selected}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div>
-            {
-                hasEnded ? <>
-                
-                </>
-                :
-                <>
-                    <button
-                        className='mx-6 sm:mx-0 mt-6 button-primary'
-                        onClick={() => {
-                            store.endTest();
-                        }}
-                    >Befejezés</button>
-                </>
-            }
-            
-        </div>
-		<div className='w-full fixed bottom-0 left-0 p-1 text-center bg-slate-200'>
-			<button className="button-primary m-2" onClick={() => { store.toggleMarked(false) }}>Nem</button>
-			<button className="button-primary m-2" onClick={() => { store.toggleMarked(true) }}>Igen</button>
-		</div>
-    </>;
+      </div>
+      <div>
+        {hasEnded ? (
+          <></>
+        ) : (
+          <>
+            <button
+              className="mx-6 sm:mx-0 mt-6 button-primary"
+              onClick={() => {
+                store.endTest();
+              }}
+            >
+              Befejezés
+            </button>
+          </>
+        )}
+      </div>
+      <div className="w-full fixed bottom-0 left-0 p-1 text-center bg-slate-200">
+        <button
+          className="button-primary m-2"
+          onClick={() => {
+            store.toggleMarked(false);
+          }}
+        >
+          Nem
+        </button>
+        <button
+          className="button-primary m-2"
+          onClick={() => {
+            store.toggleMarked(true);
+          }}
+        >
+          Igen
+        </button>
+      </div>
+    </>
+  );
 }
--- src/pages/ToulousePieron.tsx
+++ src/pages/ToulousePieron.tsx
@@ -1,175 +1,199 @@
-import React, { useState, useEffect } from 'react';
-import { COL_COUNT, MAX_TIME, ROW_COUNT } from './ToulousePieron/Constants';
-import { Corner } from './ToulousePieron/Corner';
-import { Side } from './ToulousePieron/Side';
-import { useTestStore } from './ToulousePieron/store';
-import { formatTime } from '../util';
-import { Picture } from './ToulousePieron/Picture';
-import { Timer } from '../components/Timer';
-import { GridItem } from './ToulousePieron/GridItem';
-import shallow from 'zustand/shallow'
-import { Score } from './ToulousePieron/Score';
-import Navbar from '../components/Navbar';
-import { useTranslation } from 'react-i18next';
+import { MAX_TIME } from "./ToulousePieron/Constants";
+import { useTestStore } from "./ToulousePieron/store";
+import { Picture } from "./ToulousePieron/Picture";
+import { Timer } from "../components/Timer";
+import { GridItem } from "./ToulousePieron/GridItem";
+import shallow from "zustand/shallow";
+import { Score } from "./ToulousePieron/Score";
+import Navbar from "../components/Navbar";
+import { useTranslation } from "react-i18next";
 
-export function ToulousePieronPage(){
-    return <>
+export function ToulousePieronPage() {
+  return (
+    <>
       <Navbar />
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <ToulousePieron />
         </div>
       </main>
-    
     </>
+  );
 }
 
-export function ToulousePieron(){
+export function ToulousePieron() {
+  const [t, i18n] = useTranslation("common");
 
-    const [t, i18n] = useTranslation('common');
+  let store = useTestStore(
+    ({
+      startTime,
+      endTime,
+      hasStarted,
+      hasEnded,
+      endTest,
+      startTest,
+      pictures,
+      picturesToFind,
+    }) => ({
+      startTime,
+      endTime,
+      hasStarted,
+      hasEnded,
+      endTest,
+      startTest,
+      pictures,
+      picturesToFind,
+    }),
+    shallow
+  );
 
-    let store = useTestStore(({startTime, endTime, hasStarted, hasEnded, endTest, startTest, pictures, picturesToFind})=>({
-        startTime, endTime, hasStarted, hasEnded, endTest, startTest, pictures, picturesToFind
-    }), shallow);
-    
-    if (!store.hasStarted || store.startTime == null){
-        let squares: number[] = [];
-        for(let i = 0; i < 8; i++){
-            if (i % 2 == 0){
-                squares.push(i);
-            } else {
-                squares.unshift(i);
-            }
-        }
-
-        return <>
-            <div className='px-6'>
-                <h1 className='mb-6'>{t('tests.toulousePieron.name')}</h1>
-                <p>{t('tests.toulousePieron.description.1')}</p>
-                <p>{t('tests.toulousePieron.description.2')}</p>
-                <div className='my-2'>
-                    { squares.map((index)=>{
-                        return <>
-                            <Picture value={index} />
-                        </>
-                    }) }
-                </div>
-                <p>{t('tests.toulousePieron.description.time')}</p>
-                <button
-                    className='mt-6 button-primary'
-                    onClick={()=>{
-                        store.startTest();
-                    }}
-                >{t('tests.actions.start')}</button>
-            </div>
-        </>
+  if (!store.hasStarted || store.startTime == null) {
+    let squares: number[] = [];
+    for (let i = 0; i < 8; i++) {
+      if (i % 2 === 0) {
+        squares.push(i);
+      } else {
+        squares.unshift(i);
+      }
     }
 
-    let hasEnded = store.hasEnded && store.endTime;    
-
-    return <>
-        <div>
-            {
-                hasEnded ? <>
-                    <Score />
-                </>
-                :
+    return (
+      <>
+        <div className="px-6">
+          <h1 className="mb-6">{t("tests.toulousePieron.name")}</h1>
+          <p>{t("tests.toulousePieron.description.1")}</p>
+          <p>{t("tests.toulousePieron.description.2")}</p>
+          <div className="my-2">
+            {squares.map((index) => {
+              return (
                 <>
-                    <div
-                        style={{
-                            flexDirection: 'row',
-                            alignItems: 'start'
-                        }}
-                        className='flex mb-6 gap-6 mx-6 sm:mx-0'
-                    >
-                        <div
-                            className='card text-center pb-2'
-                        >
-                            <p>{t('tests.toulousePieron.shapesToMark')}</p>
-                            { store.picturesToFind.map(pic=>{
-                                return <>
-                                    <Picture value={pic} />
-                                </>
-                            }) }
-                        </div>
-                        <Timer startTime={store.startTime} maxTime={MAX_TIME} />
-                    </div>
+                  <Picture value={index} />
                 </>
-            }
+              );
+            })}
+          </div>
+          <p>{t("tests.toulousePieron.description.time")}</p>
+          <button
+            className="mt-6 button-primary"
+            onClick={() => {
+              store.startTest();
+            }}
+          >
+            {t("tests.actions.start")}
+          </button>
+        </div>
+      </>
+    );
+  }
 
-            
-            
+  let hasEnded = store.hasEnded && store.endTime;
+
+  return (
+    <>
+      <div>
+        {hasEnded ? (
+          <>
+            <Score />
+          </>
+        ) : (
+          <>
             <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'stretch',
-                }}
+              style={{
+                flexDirection: "row",
+                alignItems: "start",
+              }}
+              className="flex mb-6 gap-6 mx-6 sm:mx-0"
             >
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 5,
-                    }}
-                >
-                    {store.pictures.map((row, index)=>{
-                        return <>
-                        <span
-                            className='font-sm w-8'
-                            style={{
-                                display: 'flex',
-                                flexShrink: 0,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                flexGrow: 1,
-                            }}
-                        >{index+1}</span>
-                        </>
-                    })}
-                </div>
-
-                <div
-                    style={{
-                        overflowX: 'auto',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        gap: 5,
-                    }}
-                >
-                { store.pictures.map((row, index)=>{
-                    return <div
-                        style={{
-                            display: 'flex',
-                            gap: 5,
-                        }}
-                    >
-                        {
-                            row.map((col, colIndex)=>{
-                                return <GridItem row={index} column={colIndex} />
-                            })
-                        }
-                    </div>
-                }) }
-                </div>
+              <div className="card text-center pb-2">
+                <p>{t("tests.toulousePieron.shapesToMark")}</p>
+                {store.picturesToFind.map((pic) => {
+                  return (
+                    <>
+                      <Picture value={pic} />
+                    </>
+                  );
+                })}
+              </div>
+              <Timer startTime={store.startTime} maxTime={MAX_TIME} />
             </div>
-        </div>
-        <div>
-            {
-                hasEnded ? <>
-                
-                </>
-                :
+          </>
+        )}
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "stretch",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 5,
+            }}
+          >
+            {store.pictures.map((row, index) => {
+              return (
                 <>
-                    <button
-                        className='mx-6 sm:mx-0 mt-6 button-primary'
-                        onClick={()=>{
-                            store.endTest();
-                        }}
-                    >{t('tests.actions.end')}</button>
+                  <span
+                    className="font-sm w-8"
+                    style={{
+                      display: "flex",
+                      flexShrink: 0,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexGrow: 1,
+                    }}
+                  >
+                    {index + 1}
+                  </span>
                 </>
-            }
-            
+              );
+            })}
+          </div>
+
+          <div
+            style={{
+              overflowX: "auto",
+              display: "flex",
+              flexDirection: "column",
+              gap: 5,
+            }}
+          >
+            {store.pictures.map((row, index) => {
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    gap: 5,
+                  }}
+                >
+                  {row.map((col, colIndex) => {
+                    return <GridItem row={index} column={colIndex} />;
+                  })}
+                </div>
+              );
+            })}
+          </div>
         </div>
-    </>;
+      </div>
+      <div>
+        {hasEnded ? (
+          <></>
+        ) : (
+          <>
+            <button
+              className="mx-6 sm:mx-0 mt-6 button-primary"
+              onClick={() => {
+                store.endTest();
+              }}
+            >
+              {t("tests.actions.end")}
+            </button>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
