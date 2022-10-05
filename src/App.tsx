4960d1d794b32b17c4b1035a6b4f941857d5b5ea
megyerikenez <m.kenz99@gmail.com>
2022-10-05 11:24:18 +0200

    Notification added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./css/App.css";
 
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import ApplyToJobMockup from "./pages/ApplyToJobMockUp";
 import { ChairLampPage } from "./pages/ChairLamp";
@@ -13,24 +13,38 @@ import { ToulousePieronPage } from "./pages/ToulousePieron";
 import { i18next } from "./i18n";
 import { BourdonPage } from "./pages/Bourdon";
 import { AuthGuard } from "./components/AuthGuard";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-	return (
-		<>
-			<I18nextProvider i18n={i18next}>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/tests" element={<Tests />} />
-					<Route path="/results" element={<AuthGuard component={<Results />} />} />
-					<Route path="/settings" element={<Settings />} />
-					<Route path="/signup" element={<ApplyToJobMockup />} />
-					<Route path="/tests/toulouse-pieron" element={<AuthGuard component={<ToulousePieronPage />} />} />
-					<Route path="/tests/chair-lamp" element={<AuthGuard component={<ChairLampPage />} />} />
-					<Route path="/tests/bourdon" element={<AuthGuard component={<BourdonPage />} />}  />
-				</Routes>
-			</I18nextProvider>
-		</>
-	);
+  return (
+    <>
+      <Toaster position="bottom-right" />
+      <I18nextProvider i18n={i18next}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tests" element={<Tests />} />
+          <Route
+            path="/results"
+            element={<AuthGuard component={<Results />} />}
+          />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/signup" element={<ApplyToJobMockup />} />
+          <Route
+            path="/tests/toulouse-pieron"
+            element={<AuthGuard component={<ToulousePieronPage />} />}
+          />
+          <Route
+            path="/tests/chair-lamp"
+            element={<AuthGuard component={<ChairLampPage />} />}
+          />
+          <Route
+            path="/tests/bourdon"
+            element={<AuthGuard component={<BourdonPage />} />}
+          />
+        </Routes>
+      </I18nextProvider>
+    </>
+  );
 }
 
 export default App;
--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosPromise } from "axios";
+import axios from "axios";
 import { i18next } from "./i18n";
 
 axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
@@ -7,112 +7,119 @@ axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 let API_ROOT = "https://api.dvpc.hu/api";
 
 interface MockupFormData {
-    firstName: string;
-    lastName: string;
-    email: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
-function checkToken(){
-    let search = (new URLSearchParams(window.location.search));
-    let token = search.get("token");
-    if(token){
-        localStorage.setItem("token",search.get("token") as string);
+function checkToken() {
+  let search = new URLSearchParams(window.location.search);
+  let token = search.get("token");
+  if (token) {
+    localStorage.setItem("token", search.get("token") as string);
 
-        window.history.pushState({},"",window.location.pathname);
-    }
+    window.history.pushState({}, "", window.location.pathname);
+  }
 }
 checkToken();
 
-export function getToken(){
-    checkToken();
+export function getToken() {
+  checkToken();
 
-    return localStorage.getItem("token");
+  return localStorage.getItem("token");
 }
 
-export function sendPostRequest<T>(url: string, data: Object){
-    return axios.request<T>({
-        url:API_ROOT+url,
-        method:"POST",
-        headers:{
-            Accept: "*/*",
-            "Content-Type": "application/json",
-        },
-        data:JSON.stringify({
-            ...data,
-            lang:i18next.language,
-            token:getToken(),
-        })
-    })
+export function sendPostRequest<T>(url: string, data: Object) {
+  return axios.request<T>({
+    url: API_ROOT + url,
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      ...data,
+      lang: i18next.language,
+      token: getToken(),
+    }),
+  });
 }
 
-export function sendMockupForm(args: MockupFormData){
-    return sendPostRequest("/register",args);
+export function sendMockupForm(args: MockupFormData) {
+  return sendPostRequest("/register", args);
 }
 
-export function calculateToulousePieronScore({incorrectlyIgnored, incorrectlyMarked, correctlyIgnored, correctlyMarked}: ToulousePieronResult){
-    let pictureCount = incorrectlyIgnored+incorrectlyMarked+correctlyIgnored+correctlyMarked;
-    let score = (pictureCount - (incorrectlyIgnored+incorrectlyMarked))/pictureCount;
-    return score;
+export function calculateToulousePieronScore({
+  incorrectlyIgnored,
+  incorrectlyMarked,
+  correctlyIgnored,
+  correctlyMarked,
+}: ToulousePieronResult) {
+  let pictureCount =
+    incorrectlyIgnored + incorrectlyMarked + correctlyIgnored + correctlyMarked;
+  let score =
+    (pictureCount - (incorrectlyIgnored + incorrectlyMarked)) / pictureCount;
+  return score;
 }
 
 interface ChairLampResultItem {
-    incorrectlyMarked: number;
-    incorrectlyIgnored: number;
-    correctlyMarked: number;
-    correctlyIgnored: number;
-    picturesRevised: number,
+  incorrectlyMarked: number;
+  incorrectlyIgnored: number;
+  correctlyMarked: number;
+  correctlyIgnored: number;
+  picturesRevised: number;
 }
 interface ChairLampResult {
-    startTime: Date;
-    endTime: Date;
-    values: ChairLampResultItem[],
+  startTime: Date;
+  endTime: Date;
+  values: ChairLampResultItem[];
 }
 interface ToulousePieronResult {
-    startTime: Date;
-    endTime: Date;
-    incorrectlyMarked: number;
-    incorrectlyIgnored: number;
-    correctlyMarked: number;
-    correctlyIgnored: number;
+  startTime: Date;
+  endTime: Date;
+  incorrectlyMarked: number;
+  incorrectlyIgnored: number;
+  correctlyMarked: number;
+  correctlyIgnored: number;
 }
 interface BourdonResult {
-    startTime: Date,
-    endTime: Date,
-    incorrectlyMarked: number,
-    incorrectlyIgnored: number,
-    correctlyMarked: number,
-    correctlyIgnored: number,
-    linesViewed: number,
-    charsViewed: number,
+  startTime: Date;
+  endTime: Date;
+  incorrectlyMarked: number;
+  incorrectlyIgnored: number;
+  correctlyMarked: number;
+  correctlyIgnored: number;
+  linesViewed: number;
+  charsViewed: number;
 }
 export interface Results {
-    toulousePieron: ToulousePieronResult[]
-    chairLamp: ChairLampResult[]
-    bourdon: BourdonResult[]
+  toulousePieron: ToulousePieronResult[];
+  chairLamp: ChairLampResult[];
+  bourdon: BourdonResult[];
 }
 
-function requestResults(){
-    return sendPostRequest<Results>("/results", {}).then(r=>r.data);
+function requestResults() {
+  return sendPostRequest<Results>("/results", {}).then((r) => r.data);
 }
 
 function convertUTCDateToLocalDate(date: Date): Date {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+  var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
 
-    var offset = date.getTimezoneOffset() / 60;
-    var hours = date.getHours();
+  var offset = date.getTimezoneOffset() / 60;
+  var hours = date.getHours();
 
-    newDate.setHours(hours - offset);
+  newDate.setHours(hours - offset);
 
-    return newDate;
+  return newDate;
 }
 
 function parseDate(date: string): Date {
-    return convertUTCDateToLocalDate(new Date(date));
+  return convertUTCDateToLocalDate(new Date(date));
 }
 
-function fixDate(r: any){
-    r.startTime = parseDate(r.startTime);
-    r.endTime = parseDate(r.endTime);
+function fixDate(r: any) {
+  r.startTime = parseDate(r.startTime);
+  r.endTime = parseDate(r.endTime);
 }
 
 /*export function getResults() {
@@ -132,39 +139,39 @@ function fixDate(r: any){
     });
 }*/
 
-export function getResults(){
-    return new Promise<Results>((resolve,reject)=>{
-        setTimeout(()=>{
-            resolve({
-                toulousePieron:[
-                    {
-                        startTime: new Date("2003-10-24"),
-                        endTime: new Date(),
-                        incorrectlyMarked: 15,
-                        incorrectlyIgnored: 12,
-                        correctlyMarked: 40,
-                        correctlyIgnored: 30,
-                    },
-                    {
-                        startTime: new Date("2003-9-24"),
-                        endTime: new Date(),
-                        incorrectlyMarked: 15,
-                        incorrectlyIgnored: 12,
-                        correctlyMarked: 40,
-                        correctlyIgnored: 30,
-                    },
-                    {
-                        startTime: new Date("2003-8-24"),
-                        endTime: new Date(),
-                        incorrectlyMarked: 15,
-                        incorrectlyIgnored: 12,
-                        correctlyMarked: 40,
-                        correctlyIgnored: 30,
-                    },
-                ],
-                chairLamp:[],
-                bourdon:[],
-            });
-        },1000);
-    })
+export function getResults() {
+  return new Promise<Results>((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        toulousePieron: [
+          {
+            startTime: new Date("2003-10-24"),
+            endTime: new Date(),
+            incorrectlyMarked: 15,
+            incorrectlyIgnored: 12,
+            correctlyMarked: 40,
+            correctlyIgnored: 30,
+          },
+          {
+            startTime: new Date("2003-9-24"),
+            endTime: new Date(),
+            incorrectlyMarked: 15,
+            incorrectlyIgnored: 12,
+            correctlyMarked: 40,
+            correctlyIgnored: 30,
+          },
+          {
+            startTime: new Date("2003-8-24"),
+            endTime: new Date(),
+            incorrectlyMarked: 15,
+            incorrectlyIgnored: 12,
+            correctlyMarked: 40,
+            correctlyIgnored: 30,
+          },
+        ],
+        chairLamp: [],
+        bourdon: [],
+      });
+    }, 1000);
+  });
 }
