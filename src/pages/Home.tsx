056f72a0df6f9bbb343f34aba80427f929d3b441
Gábor Kriskó <gabor.krisko@gmail.com>
2022-10-06 04:15:45 +0200

    width

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
     <>
       <Navbar />
       <main>
-        <div className="max-w-7xl mx-auto py-6 px-6 lg:px-8 flex items-center justify-center flex-col w-full min-h-screen absolute top-0">
+        <div className="mx-auto py-6 px-6 lg:px-8 flex items-center justify-center flex-col w-full min-h-screen absolute top-0">
           <h1 className="text-center">{t('homepage.name')}</h1>
           <h2 className="font-normal my-4 w-full text-center max-w-7xl lg:px-15">
             <div className="w-2/3 md:w-1/3 mx-auto">{t('homepage.text')}</div>
